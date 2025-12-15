6a46bb34cfd89309296c574a8f75066cb27fc170
Satyam <nikamsatyam187@gmail.com>
2025-12-15 13:19:23 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import Login from "./Login"; // correct path
-
-export default function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  return (
+import React,{useState} from "react";
+import './../styles/App.css';
+import LoginForm from './LoginForm'
+const App = () => {
+  const [isLoggedIn,setIsLoggedIn] = useState(false);
+  return ( 
     <div>
-      {!isLoggedIn ? (
-        <Login isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-      ) : (
-        <h2>You are logged in</h2>
-      )}
+      <h1>Parent Component</h1>
+        {/* Do not remove the main div */}
+        <LoginForm isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+
     </div>
-  );
+  )
 }
+
+export default App
